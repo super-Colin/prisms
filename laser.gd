9fb7d1c8cccca757b257c407dc7a5decd96b9b6b
superColin <supercolin.dev@gmail.com>
2025-03-15 19:54:19 -0400

    laser extends and bends

--- laser.gd
+++ laser.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+@export var wavelenghtSpectrumMin:Vector2= Vector2.ZERO
+@export var wavelenghtSpectrumMax:Vector2= Vector2.ZERO
 
 
 func _ready() -> void:
@@ -9,16 +10,52 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var colliding = $RayCast2D.is_colliding()
-	#print(colliding)
-	if colliding:
-		var collider = $RayCast2D.get_collider()
-		#print("laser - collider: ", collider)
-		#print($RayCast2D.get_collision_normal())
-		#print($RayCast2D.get_collision_point())
-		#createRefractionRaycast($RayCast2D.get_collision_point())
-		if "createRefractionRaycast" in collider:
-			collider.createRefractionRaycast($RayCast2D)
+	var collider = $RayCast2D.get_collider()
+	#if collider != null && "reflectInternal" in collider:
+	if collider != null:
+		#if collider.reflectInternal:
+			#collider.handleRefraction($RayCast2D)
+			if not extended:
+				#extendedRay = extendRay($RayCast2D.get_collision_point(), $RayCast2D.get_collision_normal())
+				extendedRay = extendRay($RayCast2D)
+				extendedRay.collision_mask = 2
+				$'.'.add_child(extendedRay)
+				extended = true
+			collider = extendedRay.get_collider()
+			if collider != null:
+				if not extended2:
+					#extendedRay2 = extendRay(extendedRay.get_collision_point(), extendedRay.get_collision_normal())
+					extendedRay2 = extendRay(extendedRay)
+					$'.'.add_child(extendedRay2)
+					extended2 = true
+					print("laser - fully extended")
+				
+
+
+
+
+var extended = false
+var extended2 = false
+var extendedRay
+var extendedRay2
+#func extendRay(contactPoint:Vector2, contactNormal:Vector2):
+	##var newRay = RayCast2D.new()
+	#var newRay = $RayCast2D.duplicate()
+	#newRay.collide_with_areas = true
+	#newRay.position = contactPoint - $'.'.position
+	#newRay.target_position = newRay.target_position.rotated(-PI/24)
+	#print("laser - extended: ", contactPoint)
+	#return newRay
+func extendRay(theRay):
+	var newRay = RayCast2D.new()
+	#var newRay = theRay.duplicate()
+	newRay.collide_with_areas = true
+	#newRay.target_position = Vector2(100, 0)
+	newRay.position = theRay.get_collision_point() - $'.'.position
+	newRay.target_position = theRay.target_position.rotated(-PI/24)
+	print("laser - extended: ", theRay.get_collision_point())
+	return newRay
+
 
 
 
--- prism.gd
+++ prism.gd
@@ -1,11 +1,17 @@
-extends Area2D
+extends Node2D
 
+
+@export var splits = 1
+@export var angle = 90
 
 var calledRefract = false
 var notCalledRefractFrames = 0
+var incomingRay
 var internalRay:RayCast2D
 var externalRay:RayCast2D
 
+
+var reflectInternal = true
 
 
 
@@ -13,48 +19,58 @@ var externalRay:RayCast2D
 
 
 func _physics_process(delta: float) -> void:
-	createRefractionRaycast()
-	#if internalRay:
-		#var colliding = internalRay.is_colliding()
-		#print("prism - internal collision: ", colliding, ", internal ray: ", internalRay)
-		#if colliding:
-			#var collider = internalRay.get_collider()
-			##createRefractionRaycast(internalRay.get_collision_point())
-			#notCalledRefractFrames += 1
-			#if "createRefractionRaycast" in collider:
-				#collider.createRefractionRaycast(internalRay, true)
+	#createRefractionRaycast()
+	if internalRay != null:
+		var colliding = internalRay.is_colliding()
+		if colliding:
+			var collider = internalRay.get_collider()
+			if "createRefractionRaycast" in collider:
+				collider.createRefractionRaycast(internalRay)
 
 
-#func createRefractionRaycast(ray:RayCast2D)->void:
-func createRefractionRaycast(ray = null)->void:
-	if notCalledRefractFrames >= 40 and ray != null:
-		if internalRay != null:
-			internalRay.queue_free()
-			if externalRay != null:
-				externalRay.queue_free()
-		return
-	elif ray == null:
-		notCalledRefractFrames += 1
-		return
-	calledRefract = true
-	#print("prism - ray is: ", ray)
-	#print("prism - global position is: ", $'.'.global_position)
-	var currentRay
-	if ray == internalRay:
-		print("prism - creating EXternal refraction")
-		externalRay = RayCast2D.new()
-		$'.'.add_child(externalRay)
-		currentRay = externalRay
-	#elif not internalRay:
-	else:
-		print("prism - creating internal refraction")
-		internalRay = RayCast2D.new()
-		$'.'.add_child(internalRay)
-		currentRay = internalRay
-		currentRay.collision_mask = 2
-	currentRay.collide_with_areas = true
-	currentRay.position = ray.get_collision_point() - $'.'.global_position
-	currentRay.target_position = ray.target_position.rotated(-PI/24)
+
+#func handleRefraction(ray):
+	#createRefractionRaycast(ray)
+	##if ray == incomingRay:
+	#if ray == internalRay or ray == externalRay:
+		#print("prism - is incoming ray")
+	#else:
+	##pass
+#
+#
+#
+#
+##func createRefractionRaycast(ray:RayCast2D)->void:
+#func createRefractionRaycast(ray = null)->void:
+	##if notCalledRefractFrames >= 240 and ray != null:
+		##if internalRay != null:
+			##internalRay.queue_free()
+			##if externalRay != null:
+				##externalRay.queue_free()
+		##return
+	#if ray == null:
+		##notCalledRefractFrames += 1
+		#return
+	##calledRefract = true
+	##print("prism - ray is: ", ray)
+	##print("prism - global position is: ", $'.'.global_position)
+	#var newRay = RayCast2D.new()
+	##if internalRay:
+		##print("prism - creating EXternal refraction")
+		##externalRay = RayCast2D.new()
+		##$'.'.add_child(externalRay)
+		##currentRay = externalRay
+	##if not internalRay:
+	##else:
+	#print("prism - creating internal refraction")
+	#print("prism - internal ray: ", internalRay, ", != ", ray)
+	#internalRay = RayCast2D.new()
+	#$'.'.add_child(internalRay)
+	#currentRay = internalRay
+	#currentRay.collision_mask = 2
+	#currentRay.collide_with_areas = true
+	#currentRay.position = ray.get_collision_point() - $'.'.global_position
+	#currentRay.target_position = ray.target_position.rotated(-PI/24)
 
 func createSecondaryRefractionRaycast()->void:
 	if not externalRay:
