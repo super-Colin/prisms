37f13ef66378af437548a06e8cbc5617e5359047
superColin <supercolin.dev@gmail.com>
2025-03-15 01:29:40 -0400

    internal refraction dectects outside edge

--- laser.gd
+++ laser.gd
@@ -3,13 +3,31 @@ extends Node2D
 
 
 
-#func _ready() -> void:
-	#RayCast2D
+func _ready() -> void:
+	RayCast2D
 
 
 
 func _physics_process(delta: float) -> void:
-	print($RayCast2D.is_colliding())
-	#var collider = $RayCast2D.get_collider()
-	#if $RayCast2D.is_colliding():
-		#print(collider)
+	var colliding = $RayCast2D.is_colliding()
+	#print(colliding)
+	if colliding:
+		var collider = $RayCast2D.get_collider()
+		#print("laser - collider: ", collider)
+		#print($RayCast2D.get_collision_normal())
+		#print($RayCast2D.get_collision_point())
+		#createRefractionRaycast($RayCast2D.get_collision_point())
+		if "createRefractionRaycast" in collider:
+			collider.createRefractionRaycast($RayCast2D)
+
+
+
+#func createRefractionRaycast(point:Vector2)->void:
+	#print("laser - point is: ", point)
+	#print("laser - global position is: ", $'.'.global_position)
+
+
+#(-0.869837, -0.49334)
+#(377.2537, 164.0)
+#(-0.869837, -0.49334)
+#(377.2537, 164.0)
